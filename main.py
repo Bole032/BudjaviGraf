beb0efba2d992422cab2d4fea91a63f89c8835fb
pavle.ciric <pavle.ciric@hotmail.com>
2018-07-04 22:51:50 +0200

    [Add] Chance calculation

--- main.py
+++ main.py
@@ -18,7 +18,28 @@ class Graph:
     def getCommon(self, node1, node2):
         "Return list of common nodes"
         return list(set(self.graph[node1]).intersection(self.graph[node2]))
-        
+
+    def getUnion(self, node1, node2):
+        "Return list of union nodes"
+        return list(set().intersection(self.graph[node1], self.graph[node2]))
+
+class Chance:
+    def __init__(self, node1, node2, chance):
+        self.node1 = node1
+        self.node2 = node2
+        self.chance = chance
+
+    def getChance(self):
+        return self.chance
+
+def calculateChance_1(graph):
+    chances = []
+    for i in range(len(graph.graph)):
+        for j in range(i, i + 1):
+            if j not in graph.graph[i]:
+                chances.append(Chance(i, j, graph.getCommon(i, j)))
+
+
 graph = Graph(5)
 graph.addEdge(0,4)
 print(graph.graph)
